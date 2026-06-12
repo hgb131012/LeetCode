61b3453d6242dae9bc968125a53738f99c8b25fe
Gia Bảo <hgb131012@gmail.com>
2026-06-12 15:32:53 +0700

    Create Fibonacci_Number_.py

---
+++ Fibonacci_Number_.py
@@ -0,0 +1,7 @@
+from functools import cache
+class Solution:
+    @cache
+    def fib(self, n: int) -> int:
+        if n > 1:
+            return self.fib(n - 1) + self.fib(n - 2)
+        return n
